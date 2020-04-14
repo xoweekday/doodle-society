fa7a049786c8be222ae0c3f6d09257f05f6c0648
jwebber18 <jeremy.j.webber@gmail.com>
2020-04-14 17:05:27 -0500

    added images and doodles tables to schema, added queries for users, friends, images, doodles

--- doodle-society/schema.sql
+++ doodle-society/schema.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS friends;
+DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS doodles;
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
@@ -11,4 +13,19 @@ CREATE TABLE friends (
   id serial PRIMARY KEY,
   user_id int NOT NULL,
   friend_id int NOT NULL
+);
+
+CREATE TABLE images (
+  id serial PRIMARY KEY,
+  url VARCHAR (255) NOT NULL,
+  uploader_id int NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE doodles (
+  id serial PRIMARY KEY,
+  url VARCHAR (255) NOT NULL,
+  original_id int NOT NULL,
+  doodler_id int NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
--- doodle-society/server/db/index.js
+++ doodle-society/server/db/index.js
@@ -7,12 +7,101 @@ const pool = new Pool({
   port: 5432,
 });
 
+//  get all the users
+const getUsers = (req, res) => {
+  return pool.query('SELECT * FROM users ORDER BY id ASC');
+}
+
+//retrieve a user by their id number
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  return pool.query('SELECT * FROM users WHERE id = $1', [id]);
+}
+
+//add a user to the db
 const createUser = (req, res) => {
   const { username, fullname } = req.body;
 
   return pool.query('INSERT INTO users (username, fullname) VALUES ($1, $2) RETURNING id', [username, fullname]);
 }
 
+//  add a friend relation to the db
+//  only reciprocal relationships will be true friends
+const addFriend = (req, res) => {
+  const {user_id, friend_id} = req.body;
+
+  return pool.query('INSERT INTO friends (user_id, friend_id) VALUES ($1, $2)', [user_id, friend_id]);
+}
+
+//  get all friends associated with an id that have added that id back
+const getFriends = (req, res) => {
+  const { id } = req.params;
+  //  get all user id's that user has added as friends
+  return pool.query('SELECT friend_id FROM friends WHERE user_id = $1', [id])
+    .then((friends) => {
+      //  of these id's, get all that have added user back
+      return Promise.all(friends.rows.map(friend => {
+        return pool.query('SELECT user_id FROM friends WHERE user_id = $1 AND friend_id = $2', [friend.friend_id, id]);
+      }));
+    })
+    .then((confirmedFriends) => {
+      //  filter out any empty results, then map to user id's
+      const confirmedFriendIds = confirmedFriends
+      .filter(cF => cF.rowCount)
+      .map(cF => cF.rows[0].user_id);
+      //  get all the users associated with confirmed id's
+      return Promise.all(confirmedFriendIds.map(cFId => {
+        return pool.query('SELECT * FROM users WHERE id = $1', [cFId]);
+      }));
+    });
+}
+
+//  this will return anyone who has added the user as a friend
+//  whatever id's in the results are NOT present in the user's friends (stored client-side), will be the pending
+//  requests
+const getFriendRequests = (req, res) => {
+  const { id } = req.params;
+  return pool.query('SELECT user_id FROM friends WHERE friend_id = $1', [id]);
+}
+
+const removeFriend = (req, res) => {
+  const { user_id, friend_id } = req.body;
+  return pool.query('DELETE FROM friends WHERE user_id = $1 AND friend_id = $2', [user_id, friend_id]);
+}
+
+const addImage = (req, res) => {
+  const { url, uploader_id } = req.body;
+  return pool.query('INSERT INTO images (url, uploader_id) VALUES ($1, $2) RETURNING id', [url, uploader_id]);
+}
+
+const addDoodle = (req, res) => {
+  const { url, original_id, doodler_id } = req.body;
+  return pool.query('INSERT INTO doodles (url, original_id, doodler_id) VALUES ($1, $2) RETURNING id', 
+  [url, original_id, doodler_id]);
+}
+
+const getUserUploads = (req, res) => {
+  const { id } = req.params;
+  return pool.query('SELECT * FROM images WHERE uploader_id = $1', [id]);
+}
+
+const getUserDoodles = (req, res) => {
+  const { id } = req.params;
+  return pool.query('SELECT * FROM doodles WHERE doodler_id = $1', [id]);
+}
+
+
 module.exports = {
+  getUsers,
+  getUserById,
   createUser,
+  addFriend,
+  getFriends,
+  getFriendRequests,
+  removeFriend,
+  addImage,
+  addDoodle,
+  getUserUploads,
+  getUserDoodles,
 }
