0b729c6ae761cab0252e4cda08104c8cb4601735
Harley Padua <harleypadua@gmail.com>
2020-04-14 22:53:00 -0500

    implemented upload widget (can only add from computer or url. uploads to cloudinary account)

--- doodle-society/src/App.js
+++ doodle-society/src/App.js
@@ -1,9 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { CloudinaryContext, Image } from "cloudinary-react";
+import { fetchPhotos, openUploadWidget } from "./CloudinaryService";
 import axios from 'axios';
 import './App.css';
 
 function App() {
   const [text, setText] = useState('hello');
+  const [images, setImages] = useState([]);
+
+  const beginUpload = tag => {
+    const uploadOptions = {
+      cloudName: 'dmxywbm74',
+      tags: [tag, 'anImage'],
+      uploadPreset: "xutz4j2r"
+    };
+    openUploadWidget(uploadOptions, (error, photos) => {
+      if (!error) {
+        console.log(photos);
+        if(photos.event === 'success'){
+          setImages([...images, photos.info.public_id])
+        }
+      } else {
+        console.log(error);
+      }
+    })
+  }
+  useEffect( () => {
+    fetchPhotos("image", setImages);
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -29,6 +53,17 @@ function App() {
             .then(id => console.log(id.data))
             .catch(err => console.error(err));
         }}>new user</button>
+         <CloudinaryContext cloudName='dmxywbm74'>
+        <button onClick={() => beginUpload("image")}>Upload Image</button>
+      <section>
+        {images.map(i => <Image
+              key={i}
+              publicId={i}
+              fetch-format="auto"
+              quality="auto"
+            />)}
+      </section>
+        </CloudinaryContext>
       </header>
     </div>
   );
