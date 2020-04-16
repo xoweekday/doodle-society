bd89d9e3daf9a4c90ac21fa96ad5934515124d52
Kenny-QD <KennyQDang@gmail.com>
2020-04-16 15:08:30 -0500

    renders users name and image upon login

--- doodle-society/src/Login/log-in.js
+++ doodle-society/src/Login/log-in.js
@@ -4,8 +4,12 @@ import {GoogleLogin} from 'react-google-login';
 import axios from 'axios';
 
 const Login = (props) => {
+    const [ name, setName] = useState("");
+    const [ url, setUrl] = useState("");
     const { setUser } = props;
     const responseGoogle = (response) => {
+        setName(response.profileObj.name);
+        setUrl(response.profileObj.imageUrl);
         axios.post('/api/users', response.profileObj)
             .then(id => {
                 return axios.get(`/api/users/${id.data}`);
@@ -18,45 +22,18 @@ const Login = (props) => {
       }
 
     return (
-        // <form>
-        //     <h3>Sign In</h3>
-        //         <div className="form-group">
-        //             <label>Username:</label>
-        //             <input type="email" className="form-control" placeholder="Enter username" />
-        //         </div>
-        //             <div className="form-group">
-        //                 <label>Password:</label>
-        //                 <input type="password" className="form-control" placeholder="Enter password" />
-        //             </div>
-        //             <div className="form-group">
-        //                 <div className="custom-control custom-checkbox">
-        //                     <input type="checkbox" className="custom-control-input" id="customCheck1" />
-        //                     <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-        //                 </div>
-        //             </div>
-        //             <button type="submit" className="btn btn-primary btn-block">Submit</button>
-        //                 <div>
-        //                 <GoogleLogin
-        //                     clientId="847322546124-r3jf05c1p89vlk3g6jbrbsv0632mh4go.apps.googleusercontent.com"
-        //                     buttonText="Login"
-        //                     onSuccess={responseGoogle}
-        //                     onFailure={responseGoogle}
-        //                     cookiePolicy={'single_host_origin'}
-        //                 />          
-        //                 </div>
-        //             <p className="forgot-password text-right">
-        //                 Forgot <a href="#">password?</a>
-        //             </p>
-        // </form>
-        <div>
+        <h1>{name}
+        <h2><img src={url} alt={name}/></h2>
+            <div>
             <GoogleLogin
                 clientId="847322546124-r3jf05c1p89vlk3g6jbrbsv0632mh4go.apps.googleusercontent.com"
                 buttonText="Login"
                 onSuccess={responseGoogle}
                 onFailure={responseGoogle}
                 cookiePolicy={'single_host_origin'}
-            />       
-        </div>
+                />       
+            </div>
+        </h1>
     )
 };
 
