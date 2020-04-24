004d31b6608bd2745a9f397a4e6eb9bdd2a6455c
xoweekday <xoweekday@gmail.com>
2020-04-24 14:49:02 -0500

    working on photo gallery

---
+++ src/Photo-Gallery/photo-gallery.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+
+const PhotoGallery = ({ likedDoods }) => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  const [photo, setPhotos] = useState(likedDoods);
+  
+  
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+  
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+  
+  // const photos = (likedDoods) => {
+  //   return likedDoods.map((dood) => {
+  //     return {
+  //       src: dood.url,
+  //       width: 1,
+  //       height: 1,
+  //     }
+  //   })
+  // }
+
+    console.log(photo)
+  // useEffect(() => {
+  //   photo.map((dood) => {
+  //     return {
+  //       src: dood.url,
+  //       width: 1,
+  //       height: 1,
+  //     }
+  //   })
+  // }, [photo])
+
+  return (
+    <div>
+      {/* <Gallery photos={photo}  onClick={openLightbox}/>
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photo.map(x => ({
+                ...x,
+                srcset: x.count,
+                caption: x.caption,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>  */}
+    </div>
+  );
+}
+
+export default PhotoGallery;
