c6a6dff59106294575da188d4f750853d5af1b1c
Kenny-QD <KennyQDang@gmail.com>
2020-04-25 15:05:16 -0500

    refactored comment, search, bio

--- src/Comments/CommentForm.js
+++ src/Comments/CommentForm.js
@@ -1,61 +1,66 @@
+/* eslint-disable max-lines-per-function */
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { Button, Comment, Form, Segment, List } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 const moment = require('moment');
 
-const Comments = ({user, dood, getComment}) => {
-    const[comments, setComments] = useState([]);
-    const[comment, setComment] = useState("");
-    const[showComments,setShowComments ] = useState(3);
+const Comments = ({user, dood }) => {
+    const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState("");
+    const [showComments, setShowComments ] = useState(3);
     const [fetchComments, setFetch] = useState();
+    
+    const getComments = () => axios.get(`/api/comments/${dood.id}`)
+        .then(results => setComments(results.data))
+        .catch(err => console.error(err))
 
     useEffect(() => {
-        if(fetchComments) {
+        if (fetchComments) {
             clearInterval(fetchComments);
         }
         getComments();
         setFetch(setInterval(getComments, 5000));
-    },[]);
+    }, []);
     
-    const getComments = () => {
-        return axios.get(`/api/comments/${dood.id}`)
-        .then((results) => setComments(results.data))
-        .catch(err => console.error(err));
-    }
 
     const addComments = () => {
-        if(!comment){
+        if (!comment) {
             return;
         }
-        axios.post('/api/comments', {doodle_id: dood.id, comment: comment, user_id: user.id})
+        axios.post('/api/comments', {"doodle_id": dood.id,
+            comment,
+            "user_id": user.id})
         .then(() => {
             if (comments.length > 2) {
                 setShowComments(comments.length + 1);
             }
+            
             return getComments();   
         })
         .catch(err => console.error(err));
     }
 
-    return(
+    return (
         <Comment.Group>
             <List>
-                {!!comments.length && <b>Comments ({comments.length})</b>}
+                {Boolean(comments.length) && <b>Comments ({comments.length})</b>}
                 <hr></hr>
             </List>
-            {!!showComments && !!comments.length && 
-            <div className ='hideComments' onClick={() => setShowComments(0)}>hide comments</div>}
-            <Segment.Group piled style={{overflow: 'auto', maxHeight: 550}}>
-            {comments.slice(0, showComments).map((comment) => (
-            <Comment>
+            {Boolean(showComments) && Boolean(comments.length) && 
+            <div className ="hideComments" onClick={() => setShowComments(0)}>hide comments</div>}
+            <Segment.Group piled style={{"overflow": 'auto',
+                "maxHeight": 550}}>
+            {comments.slice(0, showComments).map(comment => <Comment>
                 <Comment.Avatar src={comment[0].avatar}/>
                 <Comment.Content>
-                <Link className="userName" to={{pathname: '/profile', user: comment[1]}}> {comment.name}
-                    <Comment.Author as='a'><b>{comment[0].username}</b></Comment.Author> 
+                <Link className="userName" to={{"pathname": '/profile',
+                    "user": comment[1]}}> {comment.name}
+                    <Comment.Author as="a"><b>{comment[0].username}</b></Comment.Author> 
                     </Link>
                     <Comment.Metadata>
-                    <div><font className="createdAt">{moment(comment[0].created_at).startOf('minute').fromNow()}</font></div>
+                    <div><font className="createdAt">{moment(comment[0].created_at).startOf('minute')
+                    .fromNow()}</font></div>
                     </Comment.Metadata>
                     <Comment.Text>{comment[0].comment}</Comment.Text>
                     <Comment.Actions>
@@ -63,19 +68,21 @@ const Comments = ({user, dood, getComment}) => {
                     </Comment.Actions>
                     <hr></hr>
                 </Comment.Content>
-            </Comment>
-            ))
+            </Comment>)
             }
             </Segment.Group>
-            {!!comments.length && comments.length > showComments && <div className="headComment" dividing onClick={() => setShowComments(showComments + comments.length)}>
+            {Boolean(comments.length) && comments.length > showComments && <div className="headComment" dividing onClick={() => setShowComments(showComments + comments.length)}>
                  Show More Comments 
             </div>}
             <Form reply>
-                <Form.TextArea id='comment' onChange={(e) =>{setComment(e.target.value)}}/>
-                <Button onClick={() => addComments()} content='Reply' labelPosition='left' primary />
+                <Form.TextArea id="comment" onChange={(e) => {
+                    setComment(e.target.value) 
+                }}/>
+                <Button onClick={() => addComments()} content="Reply" labelPosition="left" primary />
             </Form>
             
         </Comment.Group>
     )
 };
+
 export default Comments;
