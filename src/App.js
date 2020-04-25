75ce6873ca93526f434b08e42bd4f952f6badbca
jwebber18 <jeremy.j.webber@gmail.com>
2020-04-25 13:25:51 -0500

    linted App.js

--- src/App.js
+++ src/App.js
@@ -22,64 +22,51 @@ function App() {
   const [doods, setDoods] = useState({});
   const [friends, setFriends] = useState([]);
   const [requests, setRequests] = useState([]);
-  const [fetchDoods, setFetchDoods] = useState();
-  const [fetchRequests, setFetchRequests] = useState();
   const [likedDoods, setLikedDoods] = useState([]);
   const [loadingDoods, setLoading] = useState(true);
 
+  const getDoods = (user) => axios.get(`/api/doodles/${user.id}`);
 
+  const getImgs = (user) => axios.get(`/api/images/${user.id}`);
 
-
-  const getDoods = (user) => {
-    return axios.get(`/api/doodles/${user.id}`);
-  }
-
-  const getImgs = (user) => {
-    return axios.get(`/api/images/${user.id}`);
-  }
-
-  const getLikedDoods = (user) => {
-    return axios.get(`/api/doodles/likes/${user.id}`)
+  const getLikedDoods = (user) => axios.get(`/api/doodles/likes/${user.id}`)
     .then((likedDoods) => {
       setLikedDoods(likedDoods.data);
-    })
-  }
+    });
 
   const getAllDoods = () => {
     if (!user.id) {
       return;
     }
     const allUsers = [user].concat(friends);
-    return Promise.all(allUsers.map(user => getDoods(user)))
+    return Promise.all(allUsers.map((user) => getDoods(user)))
       .then((allDoods) => {
         const newDoods = {};
         allDoods
-        .map(userDoods => userDoods.data)
-        .forEach(userDoods => {
-          if(userDoods.length) {
-            newDoods[userDoods[0].doodler_id] = userDoods;
-          }
-        });
+          .map((userDoods) => userDoods.data)
+          .forEach((userDoods) => {
+            if (userDoods.length) {
+              newDoods[userDoods[0].doodler_id] = userDoods;
+            }
+          });
         setDoods(newDoods);
         setLoading(false);
       })
-      .catch(err => console.error(err));
-  }
+      .catch((err) => console.error(err));
+  };
 
-  const getFriends = (user) => {
-    return axios.get(`/api/friends/${user.id}`);
-  }
+  const getFriends = (user) => axios.get(`/api/friends/${user.id}`);
 
   const getRequests = () => {
     if (!user.id) {
       return;
     }
     axios.get(`/api/friends/requests/${user.id}`)
-      .then((requests) => setRequests(requests.data.filter(request => {
-        return !friends.some(friend => friend.id === request.id);
-      })))
-      .catch(err => console.error(err));
-  }
+      .then((requests) => setRequests(requests.data
+        .filter((request) => !friends
+          .some((friend) => friend.id === request.id))))
+      .catch((err) => console.error(err));
+  };
 
   useEffect(() => {
     getAllDoods();
@@ -88,94 +75,103 @@ function App() {
   }, [friends]);
 
   useEffect(() => {
-    if(user.id) {
-        getFriends(user)
-        .then(results => {
-          setFriends(results.data);
-          })
-        .catch(err => console.error(err));
+    if (user.id) {
+      getFriends(user)
+        .then((results) => setFriends(results.data))
+        .catch((err) => console.error(err));
 
-        setInterval(() => {
-          getFriends(user)
-          .then(results => {
-            setFriends(results.data)
-          })
-          .catch(err => console.error(err));
-        }, 5000);
+      setInterval(() => {
+        getFriends(user)
+          .then((results) => setFriends(results.data))
+          .catch((err) => console.error(err));
+      }, 5000);
     }
   }, [user]);
-
-  
 
   return (
     <div className="App">
       <React.Fragment>
         <Router>
-          <ReactNotifications/>
-          <NavigationBar user={user} setUser={setUser} getAllDoods={getAllDoods} />
+          <ReactNotifications />
+          <NavigationBar
+            user={user}
+            setUser={setUser}
+            getAllDoods={getAllDoods}
+          />
           <Switch>
             <Route
-            exact path="/"
-            render={(props) => {
-              const { back } = props.location
-              if(!user.id) {
-              return <Login setUser={setUser} />
-              }
-              if(!back) {
-                return <Redirect to='/home' /> 
-              }
-              return <Redirect to={back} />
-            }
-            }
-  />
+              exact path="/"
+              render={(props) => {
+                const { back } = props.location;
+                if (!user.id) {
+                  return <Login setUser={setUser} />;
+                }
+                if (!back) {
+                  return <Redirect to="/home" />;
+                }
+                return <Redirect to={back} />;
+              }}
+            />
             <Route
               path="/upload"
               render={() => {
-                if(!user.id) {
-                  return <Redirect to={{
-                    pathname: '/',
-                    back: '/upload'
-                  }} />
+                if (!user.id) {
+                  return (
+                    <Redirect
+                      to={{
+                        pathname: '/',
+                        back: '/upload',
+                      }}
+                    />
+                  );
                 }
-                return <Upload user={user} setUser={setUser} />
+                return <Upload user={user} setUser={setUser} />;
               }}
             />
             <Route
               path="/profile"
               render={(props) => {
-                if(!user.id) {
-                  return <Redirect to={{
-                    pathname: '/',
-                    back: '/profile'
-                  }} />
+                if (!user.id) {
+                  return (
+                    <Redirect to={{
+                      pathname: '/',
+                      back: '/profile',
+                    }}
+                    />
+                  );
                 }
                 const profUser = props.location.user || user;
                 const allowEditBio = profUser.id === user.id;
                 const allowDeletePicture = profUser.id === user.id;
-                if (!friends.some(friend => friend.id === profUser.id) && profUser.id !== user.id) {
+                if (!friends.some((friend) => friend.id === profUser.id) && profUser.id !== user.id) {
                   alert(`You are not yet friends with ${profUser.name}. Please add them first.`);
-                  return <Redirect to="/home" />
+                  return <Redirect to="/home" />;
                 }
-                return <Profile
-                          user={profUser}
-                          doods={doods} 
-                          getAllDoods={getAllDoods}
-                          getImgs={getImgs}
-                          getFriends={getFriends}
-                          requests={profUser.id === user.id && requests}
-                          allowEditBio={allowEditBio}
-                          allowDeletePicture={allowDeletePicture}
-                        />
+                return (
+                  <Profile
+                    user={profUser}
+                    doods={doods}
+                    getAllDoods={getAllDoods}
+                    getImgs={getImgs}
+                    getFriends={getFriends}
+                    requests={profUser.id === user.id && requests}
+                    allowEditBio={allowEditBio}
+                    allowDeletePicture={allowDeletePicture}
+                  />
+                );
               }}
             />
             <Route
               path="/doodle"
               render={(props) => {
-                if(!user.id) {
-                  return <Redirect to={{
-                    pathname: '/',
-                    back: '/doodle'
-                  }} />
+                if (!user.id) {
+                  return (
+                    <Redirect to={{
+                      pathname: '/',
+                      back: '/doodle',
+                    }}
+                    />
+                  );
                 }
                 return (
                   <Canvas
@@ -187,16 +183,20 @@ function App() {
                 );
               }}
             />
-          <Route
-            path="/home"
-            render={() => {
-            if(!user.id) {
-              return <Redirect to={{
-                pathname: "/",
-                back: "/home"
-              }} />
-            }
-            return loadingDoods ? <div>...loading doods...</div> :
+            <Route
+              path="/home"
+              render={() => {
+                if (!user.id) {
+                  return (
+                    <Redirect to={{
+                      pathname: '/',
+                      back: '/home',
+                    }}
+                    />
+                  );
+                }
+                return loadingDoods ? <div>...loading doods...</div>
+                  : (
                     <Main
                       user={user}
                       doods={doods}
@@ -206,22 +206,29 @@ function App() {
                       likedDoods={likedDoods}
                       getAllDoods={getAllDoods}
                     />
-          }}
+                  );
+              }}
             />
             <Route
               path="/search"
               render={() => {
-                if(!user.id) {
-                  return <Redirect to={{
-                    pathname: '/',
-                    back: '/search'
-                  }} />
+                if (!user.id) {
+                  return (
+                    <Redirect to={{
+                      pathname: '/',
+                      back: '/search',
+                    }}
+                    />
+                  );
                 }
-                return <Search user={user} getFriends={getFriends} />
-            }}
-            />
+                return (
+                  <Search
+                    user={user}
+                    getFriends={getFriends}
+                  />
+                );
               }}
-              />
+            />
           </Switch>
         </Router>
       </React.Fragment>
